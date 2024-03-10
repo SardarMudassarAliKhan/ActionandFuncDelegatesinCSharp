1f20911e4c73da7268e71a8091d6983911a49efc
mudassarali.official <mudassarali.official@gmail.com>
2024-03-10 20:25:17 +0500

    2- Action delegate with no parameters

--- ActionandFuncDelegatesinCSharp/ActionandFuncDelegatesinCSharp.cs
+++ ActionandFuncDelegatesinCSharp/ActionandFuncDelegatesinCSharp.cs
@@ -4,5 +4,9 @@
     {
         // Simple Action delegate
         public Action<int, int> add = (a, b) => Console.WriteLine($"Sum: {a + b}");
+
+        // Action delegate with no parameters
+        public Action printMyName = () => Console.WriteLine("Sardar Mudassar Ali Khan!");
+
     }
 }
--- ActionandFuncDelegatesinCSharp/Program.cs
+++ ActionandFuncDelegatesinCSharp/Program.cs
@@ -5,7 +5,12 @@
         static void Main(string[] args)
         {
             ActionandFuncDelegatesinCSharp actionandFuncDelegatesinCSharp = new ActionandFuncDelegatesinCSharp();
+
+            // Invoking the Action delegate
             actionandFuncDelegatesinCSharp.add(10, 20);
+
+            // Invoking the Action delegate with no parameters
+            actionandFuncDelegatesinCSharp.printMyName();
         }
     }
 }
